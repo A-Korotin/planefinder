334154367444efde046ea791246746e330a5b95a
Alexey Korotin <sx12fx@gmail.com>
2022-07-28 22:54:00 +0300

    reactive

--- src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
+++ src/main/java/com/thehecklers/planefinder/PlaneFinderService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.net.URL;
@@ -29,7 +30,7 @@ public class PlaneFinderService {
         om = new ObjectMapper();
     }
 
-    public Iterable<Aircraft> getAircraft() throws IOException {
+    public Flux<Aircraft> getAircraft() throws IOException {
         return saveSamplePositions();
 //
 //        List<Aircraft> positions = new ArrayList<>();
@@ -64,7 +65,7 @@ public class PlaneFinderService {
 //        }
     }
 
-    private Iterable<Aircraft> saveSamplePositions() {
+    private Flux<Aircraft> saveSamplePositions() {
         repo.deleteAll();
 
         Aircraft[] aircrafts = new Aircraft[]{
--- src/main/java/com/thehecklers/planefinder/PositionReporter.java
+++ src/main/java/com/thehecklers/planefinder/PositionReporter.java
@@ -3,6 +3,7 @@ package com.thehecklers.planefinder;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,14 +15,14 @@ public class PositionReporter {
     private final PlaneFinderService pfService;
 
     @Bean
-    Supplier<Iterable<Aircraft>> reportPositions() {
+    Supplier<Flux<Aircraft>> reportPositions() {
         return () -> {
           try {
               return pfService.getAircraft();
           } catch (IOException e) {
               e.printStackTrace();
           }
-          return List.of();
+          return Flux.empty();
         };
     }
 }
